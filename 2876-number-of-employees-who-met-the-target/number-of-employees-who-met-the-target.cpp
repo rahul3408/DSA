32d544677341120f27e715123e361248e8c5ecc3
Rahul <boinipallyrahul3408@gmail.com>
2026-09-06 19:46:48 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (13.23%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count=0;
+        for(int val:hours){
+            if(val>=target){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
