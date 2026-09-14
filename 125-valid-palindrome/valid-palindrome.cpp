f6ae69134c968bbc6223bc322cab2587566adab9
Rahul <boinipallyrahul3408@gmail.com>
2026-09-14 19:49:58 +0530

    Time: 3 ms (27.88%) | Memory: 9.9 MB (52.48%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,26 +3,26 @@ public:
     bool isPalindrome(string s) {
         string s2;
         int i=0;
-        int j=0;
-        while(s[i]!='\0'){
-            if(isalnum(s[i])){
-             s2.push_back(tolower(s[i]));
-             j++;
+        int j=s.length()-1;
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
             }
-            i++;
-        }
-        int n=j;
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(s2[i]==s2[n-i-1]){
-                count++;
+            else if(!isalnum(s[j])){
+              j--;
+            }
+            else{
+            char a=tolower(s[i]);
+            char b=tolower(s[j]);
+            if(a!=b){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
             }
         }
-        if(count==n){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 };
