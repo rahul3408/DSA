286e5be6c9189c92d10d0c55682b572071ad7537
Rahul <boinipallyrahul3408@gmail.com>
2026-09-15 22:55:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.63%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        int n=address.length();
+        string s;
+        for(int i=0;i<n;i++){
+            if(address[i]=='.'){
+               s=s+"[.]";
+            }
+            else{
+                s=s+address[i];
+            }
+        }
+        return s;
+        
+    }
+};
